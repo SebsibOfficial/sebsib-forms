11e81e8d18f1f02210f3db95aa1e1db0d403d4ac
Yoseph <yosephten@gmail.com>
2023-03-01 22:00:48 +0300

    Advanced Integration

--- src/screens/Landing/Landing.tsx
+++ src/screens/Landing/Landing.tsx
@@ -6,16 +6,17 @@ import Sb_Questions, { QuestionI } from '../../components/Sb_Question/Sb_Questio
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle, faFileUpload } from '@fortawesome/free-solid-svg-icons';
-import { GetSurvey, SendResponse } from '../../utils/api';
+import { GetMetaInfo, GetSurvey, SendResponse } from '../../utils/api';
 import { useParams } from 'react-router-dom';
-import { generateId, translateIds } from '../../utils/helpers';
+import { encrypt, generateId, translateIds } from '../../utils/helpers';
 import Sb_Text from '../../components/Sb_Text/Sb_Text';
+import axios from 'axios';
 
 interface AnswerI {
   _id: string,
   inputType: "TEXT" | "CHOICE" | "MULTI-SELECT" | "NUMBER" | "FILE" | "DATE",
   questionId: string,
-  answer?: string | string[] | object
+  answer?: any
 }
 
 interface ResponsePayloadI {
@@ -25,11 +26,19 @@ interface ResponsePayloadI {
   sentDate: string
 }
 
+interface MetaInfoI {
+  responseId: string,
+  projectName: string,
+  shortOrgId: string,
+}
+
 export default function Landing() {
   let { link } = useParams();
   const [surveyId, setSurveyId] = useState("");
   const [surveyName, setSurveyName] = useState("");
   const [surveyDesc, setSurveyDesc] = useState("");
+  const [shortSurveyId, setShortSurveyid] = useState("");
+  const [metaInfo, setMetaInfo] = useState<MetaInfoI>();
   const [phase, setPhase] = useState<"INITIAL" | "FILLING" | "COMPLETE">("INITIAL")
   const [QS, setQS] = useState<QuestionI[]>([])
   const [ANS, setANS] = useState<AnswerI[]>([])
@@ -38,6 +47,7 @@ export default function Landing() {
   const [btnLoading, setBtnLoading] = useState(false);
   const [pageOk, setPageOk] = useState(true);
   const [mandatoryFilled, setMandatoryFilled] = useState(false);
+  const [fileCheckStat, setFileCheckStat] = useState(true);
 
   const handleResponse = (answer: string | string[] | object, id: string) => {
     var ANS_STATE_COPY = [...ANS]
@@ -118,8 +128,102 @@ export default function Landing() {
     return true
   }
 
-  const submitResponse = () => {
+  const fileCheck = (Answers: AnswerI[]) => {
+    for (let index = 0; index < Answers.length; index++) {
+     const element = Answers[index];
+     if (element.inputType == "FILE" && element.answer !== null && element.answer !== undefined) {
+      if (
+        !["application/msword", 
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+          "image/jpeg",
+          "video/mp4",
+          "image/png",
+          "application/pdf",
+          "application/vnd.ms-powerpoint",
+          "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+          "application/vnd.rar",
+          "application/vnd.ms-excel",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          "application/zip",
+          "text/plain"
+        ].includes(element.answer.type) || element.answer.size > 1000000)
+          return false
+     }
+    }
+    return true
+  }
+
+  const GetSetMeta = () => {
+    try {
+      if (surveyId !== "") {
+        GetMetaInfo(surveyId).then((result) => {
+          if (result.code == 200) {
+            setMetaInfo({
+              responseId: generateId(),
+              projectName: result.data.projectName,
+              shortOrgId: result.data.shortOrgId
+            })
+          } else {
+            console.log(result.data)
+          }
+        }) 
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const checkAndUploadFileType = async () => {
+    // Upload pictures, if successfull -> set answers to upload path, if no -> return 0, if no files -> return 1
+    var ANS_CPY = ANS;
+    for (let index = 0; index < ANS_CPY.length; index++) {
+      if (ANS_CPY[index].inputType === "FILE"){
+        var toBeUploaded = ANS_CPY[index].answer;
+        var nameOfFile = (toBeUploaded as any).name as string
+        var ext = nameOfFile.split(".")[nameOfFile.split(".").length - 1]
+        var formData = new FormData();
+        formData.append("file", toBeUploaded);
+        var tm = new Date().getTime();
+        try {
+          var resp = await axios({
+            method: 'post',
+            headers: {
+              secKey: encrypt(tm+'')
+            },
+            url: 'http://localhost:3003/file/upload',
+            params: {
+              ext: ext,
+              response: metaInfo?.responseId ?? '',
+              project: metaInfo?.projectName ?? '',
+              survey: shortSurveyId,
+              account: metaInfo?.shortOrgId ?? ''
+            },
+            data: formData,
+          })
+
+          if (resp.status === 200) {
+            ANS_CPY[index].answer = metaInfo?.shortOrgId+"/"+metaInfo?.projectName+"/"+shortSurveyId+"/"+metaInfo?.responseId+"/"+resp.data.fn;
+            setANS(ANS_CPY);
+          } 
+          else {
+            console.log(resp);
+            return 0
+          }
+        } catch (error) {
+          console.log(error);
+          return 0
+        }
+      }
+    }
+    return 1
+  }
+
+  const submitResponse = async() => {
     setBtnLoading(true)
+    if (!await checkAndUploadFileType()) {
+      setBtnLoading(false)
+      return console.log("FILE ERROR")
+    }
     var ResponsePayload: ResponsePayloadI = {
       _id: generateId() as string,
       surveyId: surveyId,
@@ -183,6 +287,7 @@ export default function Landing() {
       if (result.code == 200) {
         if (result.data.status == "STARTED"){
           setSurveyId(result.data._id);
+          setShortSurveyid(result.data.shortSurveyId)
           setSurveyName(result.data.name);
           setSurveyDesc(result.data.description);
           mapDisplay(result.data.questions);
@@ -201,11 +306,15 @@ export default function Landing() {
         setPageLoading(false)
         console.log(result.data)
       }
-    }).catch((err) => console.error(err)) 
+    })
+    .catch((err) => console.error(err)) 
   }, [])
+
+  useEffect(() => GetSetMeta(), [surveyId])
 
   // Check if mandatory fields are filled, listen to the Question and Answers
   useEffect(() => {
+    setFileCheckStat(fileCheck(ANS))
     setMandatoryFilled(mandatoryCheck(QS, ANS))
   })
 
@@ -228,6 +337,7 @@ export default function Landing() {
             <img src={FormsLogo} alt="" />
           </Col>
         </Row>
+        {console.log("File",fileCheckStat, "Mand", mandatoryFilled)}
         <Row>
           <Col className='middle-form'>
             <div className='forms-thumbnail'>
@@ -244,12 +354,16 @@ export default function Landing() {
                     <Button variant='primary' className='mt-3' onClick={() => setPhase("FILLING")}>Start</Button>
                   </div>
                   <div className='ques-container' style={{'display':phase == "FILLING" ? '' : 'none'}}>
+                    <div style={{'transform':'translateY(-15px)', 'opacity':'0.8'}}><Sb_Text color='--DangerRed' font={24}>* </Sb_Text><Sb_Text font={12}> Questions are required</Sb_Text></div>
                     {
                       QS.map((ques, index) => (
-                        <Sb_Questions key={index} qs={ques.qs} it={ques.it} cs={ques.cs} sh={ques.sh} disp={lookUpDisp(ques.id)} id={ques.id} rq={ques.rq} onResponse={(ans, id) => handleResponse(ans, id)}/>
+                        <>
+                          <Sb_Questions key={index} qs={ques.qs} it={ques.it} cs={ques.cs} sh={ques.sh} disp={lookUpDisp(ques.id)} id={ques.id} rq={ques.rq} onResponse={(ans, id) => handleResponse(ans, id)}/>
+                          <div style={{'display': ques.it == "FILE" && !fileCheckStat ? '': 'none', 'transform':'translateY(-35px)'}}><Sb_Text color='--DangerRed' font={12}>File Type or Size Unsuitable</Sb_Text></div>
+                        </>
                       )) 
                     }
-                    <Button variant='primary' size='sm' style={{'float':'right'}} disabled={!mandatoryFilled} onClick={ mandatoryFilled ? () => submitResponse() : () => null}>{btnLoading ? <Sb_Loader/> : "Submit Response"}</Button>
+                    <Button variant='primary' size='sm' style={{'float':'right'}} disabled={!(mandatoryFilled && fileCheckStat)} onClick={ mandatoryFilled && fileCheckStat ? () => submitResponse() : () => null}>{btnLoading ? <Sb_Loader/> : "Submit Response"}</Button>
                   </div>
                   <div style={{'display':phase == "COMPLETE" ? '' : 'none'}}>
                     <Row>
